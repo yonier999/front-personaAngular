cdcaf743d93e40de2fec24a47fb89e5f6f4d1a92
yonier999 <yonierjulian999@gmail.com>
2022-04-24 08:19:40 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListarPersonasComponent } from './components/listar-personas/listar-personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarPersonasComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/listar-personas/listar-personas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-personas',
+  templateUrl: './listar-personas.component.html',
+  styleUrls: ['./listar-personas.component.css']
+})
+export class ListarPersonasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
