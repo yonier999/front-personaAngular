6c86fb54f5f5d95d813efffe50def5443ac9479e
yonier999 <yonierjulian999@gmail.com>
2022-05-08 08:45:06 -0500

    formulario y validación

--- src/app/components/formulario-persona/formulario-persona.component.ts
+++ src/app/components/formulario-persona/formulario-persona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonaModelo } from 'src/app/modelos/Personas/persona.modelo';
 
 @Component({
   selector: 'app-formulario-persona',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-persona.component.css']
 })
 export class FormularioPersonaComponent implements OnInit {
+  persona = new PersonaModelo();
 
-  constructor() { }
+  mensaje: string ="";
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+  InsertarPersona(){
+    this.mensaje = "";
+    let validar = this.validarCampos();
+    if(validar){
+      console.log(validar);
+    }else{
+
+    }
+  }
+  validarCampos(){
+    if(this.persona.nombre == ""){
+      this.mensaje = "diligenciar el nombre";
+      return false;
+    }
+    if(this.persona.apellido == ""){
+      this.mensaje = "diligenciar el apellido";
+
+      return false;
+    }
+    if(this.persona.email == ""){
+      this.mensaje = "diligenciar el email";
+
+      return false;
+    }
+    if(this.persona.ciudad == ""){
+      this.mensaje = "diligenciar el ciudad";
+
+      return false;
+    }
+    if(this.persona.telefono == ""){
+      this.mensaje = "diligenciar el telefono";
+
+      return false;
+    }
+    return true;
   }
 
 }
